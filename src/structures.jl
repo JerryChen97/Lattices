143246e0ec7d6fe3bfb0069bfddd2916b7dc0a04
Jerry <chenys13@outlook.com>
2020-02-07 14:02:42 -0500

    debug the constructor of SquareLattice

--- src/structures.jl
+++ src/structures.jl
@@ -1,10 +1,11 @@
-mutable struct SquareLattice{Int}
+mutable struct SquareLattice
     L::Int
-    point_list = []
+    point_list
     coordinates_next
     SquareLattice(L) = (
         lat=new(); 
         lat.L = L;
+        lat.point_list = Array{Any, 2}(undef, L, L);
         lat.coordinates_next = [mod(i+1, 1:L) for i in 1:L]
     )
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,2 @@
-print("Hello")
+println("Hello")
+include("structures.jl")
