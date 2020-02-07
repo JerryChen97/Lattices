d5c0004a04eced0073cd45f0a0bfca950d89ce87
Jerry <chenys13@outlook.com>
2020-02-07 15:22:40 -0500

    format the test/structures.jl

--- test/structures.jl
+++ test/structures.jl
@@ -1,14 +1,9 @@
 using Lattices
-# lat = SquareLattice(4)
-# print_lat(lat)
 
 @testset "Square Lattice Constructor" begin
     a = rand([1, -1], 2, 2)
-    # for element in a
-    #     element = rand([1, -1])
-    # end
-    # println("a = ", a)
     @test (lat_a = SquareLattice(a); typeof(lat_a) === SquareLattice)
+
     b = rand([0, 2], 2, 2)
     @test_throws AssertionError lat_b = SquareLattice(b)
 
