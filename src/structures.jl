597033347884b7f6a4b7355303fd74e1d8927c50
Jerry <chenys13@outlook.com>
2020-02-07 14:56:04 -0500

    add another constructor

--- src/structures.jl
+++ src/structures.jl
@@ -2,12 +2,25 @@ mutable struct SquareLattice
     L::Int
     point_list
     coordinates_next
-    SquareLattice(L) = (
-        lat=new(); 
+    # initialize an arbitrary SquareLattice with a given square length
+    SquareLattice(L::Int) = (
+        lat=new();
         lat.L = L;
         lat.point_list = Array{Any, 2}(undef, L, L);
         lat.coordinates_next = [mod(i+1, 1:L) for i in 1:L];
         lat
     )
+    # initialize a specific SquareLattice with given data of specific form
+    # here we only consider the S = 1/2, S_z in {1, -1}
+    function SquareLattice(twoD_list :: Array)
+        size_of_list = size(twoD_list)
+        @assert size_of_list[1] == size_of_list[2]
+        lat=new()
+        L = size_of_list[1]
+        lat.L = L
+        lat.point_list = twoD_list
+        lat.coordinates_next = [mod(i+1, 1:L) for i in 1:L]
+        lat
+    end
 end
 
--- test/structures.jl
+++ test/structures.jl
@@ -1,3 +1,7 @@
 using Lattices
 lat = SquareLattice(4)
 print_lat(lat)
+
+a = Array{Int, 2}(undef, 2, 2)
+lat_a = SquareLattice(a)
+print_lat(lat_a)
